0cb94aef697ab456ac4e2cf15e6efbf561198ba1
yeikov <jacobo.m.f@gmail.com>
2014-08-03 13:18:01 +0200

    DEBUG=False

--- prueba/settings.py
+++ prueba/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'r(t_c=m3buuu)qi$&--hg$h@$925=r90q2jj4(i5cdl%1x6&s*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
